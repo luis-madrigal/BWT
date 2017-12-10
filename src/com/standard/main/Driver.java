acd7649ed5e5f9015e145527d385c2ef6094115e
Luis Madrigal <lmadrigal.111@gmail.com>
2017-12-10 18:16:47 +0800

    added time tracker to record execution time

--- src/com/standard/main/Driver.java
+++ src/com/standard/main/Driver.java
@@ -1,11 +1,14 @@
 package com.standard.main;
 
 import com.standard.bwt.BWTStandard;
+import com.utils.Timer;
 
 public class Driver {
 	
 	public static void main(String[] args) {
+		Timer.start();
 		new BWTStandard().process("BANANA");
+		System.out.println("Execution time for standard algorithm: " + Timer.end());
 	}
 
 }
---
+++ src/com/utils/Timer.java
@@ -0,0 +1,14 @@
+package com.utils;
+
+public class Timer {
+	
+	private static long startTime;
+	
+	public static void start() {
+		startTime = System.currentTimeMillis();
+	}
+
+	public static long end() {
+		return System.currentTimeMillis() - startTime;
+	}
+}
