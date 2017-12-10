098a8cf6aec4b299bd3ee61b9931738ccd3d3658
Luis Madrigal <lmadrigal.111@gmail.com>
2017-12-10 18:18:03 +0800

    added unit of time

--- src/com/standard/main/Driver.java
+++ src/com/standard/main/Driver.java
@@ -8,7 +8,7 @@ public class Driver {
 	public static void main(String[] args) {
 		Timer.start();
 		new BWTStandard().process("BANANA");
-		System.out.println("Execution time for standard algorithm: " + Timer.end());
+		System.out.println("Execution time for standard algorithm: " + Timer.end() + " ms");
 	}
 
 }
